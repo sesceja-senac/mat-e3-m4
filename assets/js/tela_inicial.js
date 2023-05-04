ac8664566f25552970d96d9759618eabc9ea90ce
luiz-brancher <lppbrancher@senacrs.com.br>
2023-05-04 11:47:30 -0300

    tela inicial

--- assets/js/tela_inicial.js
+++ assets/js/tela_inicial.js
@@ -1,28 +1,39 @@
-// animate__animated animate__backInRight animate__delay-1s
-$('.botao-fechar').click(function(){
-    $('.titulo').removeClass('non-visible')
-    $('.start-bt').removeClass('non-visible')
-    $('.backdrop').fadeOut()
-    $('.titulo').addClass('animate__animated animate__backInLeft animate__delay-1s')
-    window.setTimeout(function() {
-        $('.start-bt').addClass('animate__animated animate__backInRight animate__delay-1s')
-    }, 500)
-})
-$('.botao-video').click(function(){
-  $('.backdrop').fadeIn()
-  $('html').css('overflow', 'hidden')
-})
-const animated = document.querySelector(".profile-card");
-var count;
+$('.backdrop').fadeOut()
+window.addEventListener('load', function(){ 
+  $('.preloader').fadeOut();
+    // animate__animated animate__backInRight animate__delay-1s
+    $('.botao-fechar').click(function(){
+      $('.titulo').removeClass('non-visible')
+      $('.start-bt').removeClass('non-visible')
+      $('.botao-video').removeClass('non-visible')
+      $('.backdrop').fadeOut()
+      $('.start-bt').addClass('animate__animated animate__backInRight')
+      $('.botao-video').addClass('animate__animated animate__backInRight')
+      $("iframe").attr("src", $("iframe").attr("src"));
+    })
+    $('.botao-video').click(function(){
+    $('.backdrop').fadeIn()
+    $('html').css('overflow', 'hidden')
+    })
+    if(window.innerWidth<1000){
+      setTimeout(function(){
+        $('.mobile_ui').addClass('zindex')
+      }, 3000)
+    }
 
-animated.addEventListener("animationend", () => {
-  count++
-  console.log("Animation ended");
-  
-  if(count=9) {
-    setTimeout(function(){
-      $(".profile-card header").addClass('on');
-    }, 3200);
-  }
-  
+    const animated = document.querySelector(".profile-card");
+    var count;
+
+    animated.addEventListener("animationend", () => {
+    count++
+    console.log("Animation ended");
+
+    if(count=9) {
+      setTimeout(function(){
+        $(".profile-card header").addClass('on');
+      }, 3200);        
+    }
+
+    });
+    setTimeout(function(){$('.backdrop').fadeIn(); console.log('q')}, 3500)
 });
